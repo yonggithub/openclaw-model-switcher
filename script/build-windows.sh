385bb947cc3c0efe6b8189ed3ea309f10d01d5e0
john <876222665@qq.com>
2026-03-20 11:02:48 +0800

    only go

--- script/build-windows.sh
+++ script/build-windows.sh
@@ -2,7 +2,7 @@
 export PATH=$HOME/go-local/go/bin:$PATH
 cd $(dirname $0)/..
 echo "Building update_openclaw for Windows..."
-GOOS=windows GOARCH=amd64 go build -buildvcs=false -o ../build/update_openclaw.exe ./cmd/update/
+GOOS=windows GOARCH=amd64 go build -buildvcs=false -o ./build/update_openclaw.exe ./cmd/update/
 if [ $? -eq 0 ]; then
     echo "Windows build successful: update_openclaw.exe"
 else
