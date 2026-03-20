ee91ad4909c148fee46fe111b1dcb3f6fce3f935
john <876222665@qq.com>
2026-03-20 11:02:48 +0800

    only go

--- script/build-linux.sh
+++ script/build-linux.sh
@@ -2,7 +2,7 @@
 export PATH=$HOME/go-local/go/bin:$PATH
 cd $(dirname $0)/..
 echo "Building OpenClawSwitch for Linux..."
-GOOS=linux GOARCH=amd64 go build -buildvcs=false -o ../build/OpenClawSwitch-linux .
+GOOS=linux GOARCH=amd64 go build -buildvcs=false -o ./build/OpenClawSwitch-linux .
 if [ $? -eq 0 ]; then
     echo "Linux build successful: OpenClawSwitch-linux"
 else
--- script/start.sh
+++ script/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd $(dirname $0)/..
-nohup ./OpenClawSwitch-linux >/dev/null 2>&1 &
+nohup ./build/OpenClawSwitch-linux >/dev/null 2>&1 &
 echo $! > app.pid
 echo "OpenClawSwitch started."
